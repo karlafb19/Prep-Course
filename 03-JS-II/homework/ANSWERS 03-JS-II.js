36fb8871201cc513937418825f170d9a6847db21
karla <karla.falconb@hotmail.com>
2022-01-28 20:36:19 -0500

    HOMEWORK 03-JS-II

---
+++ 03-JS-II/homework/ANSWERS 03-JS-II.js
@@ -0,0 +1,4 @@
+//`for` nos ayuda a repetir un bucle de código muchas veces
+// `&&` lo usamos para decir "y" AND
+// `||` lo usamos para decir "o" OR
+// `!`  lo usamos para negar, devolverá un valor opuesto
